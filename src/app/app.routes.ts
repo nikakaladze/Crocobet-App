2b04f24f689e55db840063ab5d4b3d3149104781
nikakaladze <nikakaladze89@gmail.com>
2025-06-13 13:47:33 +0400

    feat: add route definitions with lazy loading and redirects for posts, users, and todos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        pathMatch: 'full',
+        path: 'posts',
+        loadComponent: () =>
+            import('./post-list/post-list.component').then((m) => m.PostList),
+    },
+    {
+        pathMatch: 'full',
+        path: 'posts/:id',
+        loadComponent: () =>
+            import('./post-cards/post-cards.component').then((m) => m.PostCards),
+    },
+    {
+        pathMatch: 'full',
+        path: 'users',
+        loadComponent: () =>
+            import('./users-list/users-list.component').then((m) => m.UsersList),
+    },
+    {
+        pathMatch: 'full',
+        path: 'todos/:id',
+        loadComponent: () => import('./todos/todos.component').then((m) => m.Todos),
+    },
+    {
+        pathMatch: 'full',
+        path: '',
+        redirectTo: 'posts',
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    },
+];
